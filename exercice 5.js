acc0d3ef4666c4268c17e0b9380e89edb58fcfac
Abdessabour Libouti <a.libouti8781@uca.ac.ma>
2025-12-04 12:21:46 +0100

    l'ajout exercice 5

---
+++ exercice 5.js
@@ -0,0 +1,8 @@
+function TableMultiplicationDynamique(){
+    let n = Number(prompt("Entrez un nombre : "));
+    console.log("Table de multiplication de " + n + " à 10:");
+    for(let i=1;i<=10;i++){
+        console.log(n + " x " + i + " = " + (n*i));
+    }
+}
+TableMultiplicationDynamique();
